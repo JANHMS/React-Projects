ffcb5683ef95deb67dedb081936cbe246d4dbfe3
Janneman <Jan@kooperativ.net>
2019-11-03 11:17:21 +0100

    types of props

--- Headfolder/Probs/01_Hello_Props/index.js
+++ Headfolder/Probs/01_Hello_Props/index.js
@@ -2,8 +2,19 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello to="Ringo" from="Paul" />
-        <Hello to="Cher" from="Sonny" />
+        <Hello
+        to="Ringo"
+        from="Paul"
+        num={3}
+        data={[1,2,3,4,5]}
+        ifFunny
+        bangs={4}/>
+        <Hello
+        to="Cher"
+        from="Sonny"
+        bangs={10}
+        img='https://hellopledge.files.wordpress.com/2012/07/hello_medium-file3.jpg'
+        />
       </div>
     )
   }
