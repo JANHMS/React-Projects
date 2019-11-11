f5e11ab38667f59dc6bd025eac2bcc8fcc6db245
Janneman <Jan@kooperativ.net>
2019-11-11 17:05:14 +0100

    Dice State Tutorial

--- Headfolder/statetutorial/src/App.js
+++ Headfolder/statetutorial/src/App.js
@@ -6,11 +6,12 @@ import Rando from './Rando';
 import Button from './Button';
 import Brokenclick from './Brokenclick'
 import Clicker from './Clicker'
-
+import Die from './Die'
+import RollDice from './RollDice'
 class App extends Component{
   render(){
     return <div className='App'>
-      <Clicker/>
+      <RollDice />
       </div>
   }
 }
