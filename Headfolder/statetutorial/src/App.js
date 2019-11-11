91a76322901a4876f3504a2546ce63556d1d1203
Janneman <Jan@kooperativ.net>
2019-11-11 13:36:05 +0100

    Learn changing React State

--- Headfolder/statetutorial/src/App.js
+++ Headfolder/statetutorial/src/App.js
@@ -2,12 +2,11 @@ import React,{Component} from 'react';
 import Game from './Game';
 import Demo from './Demo'
 import './App.css';
-
+import Rando from './Rando'
 class App extends Component{
   render(){
     return <div className='App'>
-      <Game color='purple'/>
-      <Demo animal='Cat' food='Pineapple'/>
+      <Rando maxNum={7}/>
       </div>
   }
 }
--- Headfolder/statetutorial/src/Game.js
+++ Headfolder/statetutorial/src/Game.js
@@ -6,14 +6,14 @@ class Game extends Component{
     this.state = {
       score:0,
       gameOver:false
-    }
+    }; 
   }
   render(){
     return(
       <div>
         <h1>Your scorcen is {this.state.score}</h1>
       </div>
-    )
+    );
   }
 }
 
